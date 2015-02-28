f11dfa91931cfeef31411976be4c7ce07bc24812
Bill <bkim54@jhu.edu>
2015-02-28 17:28:30 -0500

    tried to do tabs..not working

--- Flashboard/app/src/main/java/bkim54kzhang12/flashboard/MainActivity.java
+++ Flashboard/app/src/main/java/bkim54kzhang12/flashboard/MainActivity.java
@@ -1,17 +1,43 @@
 package bkim54kzhang12.flashboard;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
+
+    private ViewPager viewPager;
+    private TabsPagerAdapter mAdapter;
+    private ActionBar actionBar;
+    // Tab titles
+    private String[] tabs = { "Top Rated", "Games", "Movies" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initilization
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        actionBar = getActionBar();
+        mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(mAdapter);
+        //actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Adding Tabs
+        for (String tab_name : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tab_name)
+                    .setTabListener(this));
+        }
+
     }
 
 
@@ -35,5 +61,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+    }
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
     }
 }
