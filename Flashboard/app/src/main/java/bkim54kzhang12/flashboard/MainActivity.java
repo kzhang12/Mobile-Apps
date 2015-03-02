c7f5a17f519e875324f63ad91345b703e3a51ef4
Bill <bkim54@jhu.edu>
2015-03-01 20:47:18 -0500

    pager tab strip implementaiton

--- Flashboard/app/src/main/java/bkim54kzhang12/flashboard/MainActivity.java
+++ Flashboard/app/src/main/java/bkim54kzhang12/flashboard/MainActivity.java
@@ -1,43 +1,36 @@
 package bkim54kzhang12.flashboard;
 
 import android.app.ActionBar;
-import android.app.FragmentTransaction;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
-
-    private ViewPager viewPager;
-    private TabsPagerAdapter mAdapter;
-    private ActionBar actionBar;
-    // Tab titles
-    private String[] tabs = { "Top Rated", "Games", "Movies" };
+public class MainActivity extends ActionBarActivity {
+    ActionBar actionBar;
+    CustomPagerAdapter mCustomPagerAdapter;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initilization
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        actionBar = getActionBar();
-        mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
 
-        viewPager.setAdapter(mAdapter);
-        //actionBar.setHomeButtonEnabled(false);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        mCustomPagerAdapter = new CustomPagerAdapter(getSupportFragmentManager(), this);
 
-        // Adding Tabs
-        for (String tab_name : tabs) {
-            actionBar.addTab(actionBar.newTab().setText(tab_name)
-                    .setTabListener(this));
-        }
-
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mCustomPagerAdapter);
     }
 
 
@@ -63,10 +56,58 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         return super.onOptionsItemSelected(item);
     }
 
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+    private class CustomPagerAdapter extends FragmentPagerAdapter {
+        Context mContext;
+
+        public CustomPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            mContext = context;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            // Create fragment object
+            Fragment fragment = new DemoFragment();
+
+            // Attach some data to the fragment
+            // that we'll use to populate our fragment layouts
+            Bundle args = new Bundle();
+            args.putInt("page_position", position + 1);
+
+            // Set the arguments on the fragment
+            // that will be fetched in the
+            // DemoFragment@onCreateView
+            fragment.setArguments(args);
+
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page " + (position + 1);
+        }
     }
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    public static class DemoFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            // Inflate the layout resource that'll be returned
+            View rootView = inflater.inflate(R.layout.fragment_demo, container, false);
+
+            // Get the arguments that was supplied when
+            // the fragment was instantiated in the
+            // CustomPagerAdapter
+            Bundle args = getArguments();
+            ((TextView) rootView.findViewById(R.id.textView)).setText("Page " + args.getInt("page_position"));
+
+            return rootView;
+        }
     }
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-    }
+
 }
